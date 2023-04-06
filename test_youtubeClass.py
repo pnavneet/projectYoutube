f2ba61176ebb0fbb7843c2011548935bf39dc023
pnavneet <navneetpurohit1@gmail.com>
2023-04-06 11:36:12 -0700

    writing unittest for youtubeClass file

---
+++ test_youtubeClass.py
@@ -0,0 +1,16 @@
+import unittest
+from youtubeClass import Youtubedl
+
+
+class TestYoutubedl(unittest.TestCase):
+
+    def test_create_folder(self):
+        example = 'ut2'
+        # obj  = Youtubedl()
+        result = Youtubedl.createFolder(self, folder=example)
+        self.assertEqual(result, None)
+
+
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        unittest.main()
